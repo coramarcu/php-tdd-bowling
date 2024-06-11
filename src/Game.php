c33d65bb3707be458fd8ecba86dfa5c618de50c8
Cora Marcu <coramarcu@gmail.com>
2024-06-11 16:29:27 +0100

    pass testThatRollMethodCanBeCalledAMaxOf21Times()

--- src/Game.php
+++ src/Game.php
@@ -3,6 +3,7 @@
 final class Game {
     private int $total = 0;
     private int $frames = 0;
+    private int $rolls = 0;
 
     public function getTotal(): int {
         return $this->total;
@@ -12,11 +13,19 @@ final class Game {
         return $this->frames;
     }
 
+    public function getRolls(): int {
+        return $this->rolls;
+    }
+
     public function roll(int $pins_down): void {
         $this->total += $pins_down;
 
         if($this->frames < 10) {
             $this->frames++;
+        }
+
+        if($this->rolls < 21) {
+            $this->rolls++;
         }
     }
 
