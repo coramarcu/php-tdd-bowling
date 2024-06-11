f97d26e0def4ababbcb564ae67196c5960df4f28
Cora Marcu <coramarcu@gmail.com>
2024-06-11 13:38:34 +0100

    not pass testThatRollMethodUpdatesTheTotalWithPassedArgument()

--- src/Game.php
+++ src/Game.php
@@ -3,7 +3,11 @@
 final class Game {
     private int $total = 0;
 
+    public function getTotal(): int {
+        return $this->total;
+    }
+
     public function roll(int $pins_down): void {
-        
+        $this->total += $pins_down;
     }
 }
